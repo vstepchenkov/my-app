85d7fab2278d947c3448fcffb155b067e20d9651
vstepchenkov <mail2blast@gmail.com>
2020-11-17 14:44:16 +0200

    39.action

--- src/components/profile/MyPosts/MyPosts.jsx
+++ src/components/profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./MyPosts.module.css";
 import Post from "./Post/Post";
+import { addPostActionCreator, updateNewPostTextActionCreator } from "../../../redux/state";
 
 const MyPosts = (props) => {
   let postsElement = props.postsData.map((post) => (
@@ -11,16 +12,13 @@ const MyPosts = (props) => {
 
   let addPost = () => {
     //props.addPost();
-    props.dispatch({ type: "ADD-POST" })
+    props.dispatch(addPostActionCreator())
   }
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
     //props.updateNewPostText(text);
-    props.dispatch({
-      type: "UPDATE-NEW-POST-TEXT",
-      newText: text,
-    })
+    props.dispatch(updateNewPostTextActionCreator(text))
   }
 
   return (
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+
 let store = {
     _state: {
     profilePage: {
@@ -61,7 +64,7 @@ let store = {
     },
  */   
     dispatch(action) {
-        if (action.type === "ADD-POST") {
+        if (action.type === ADD_POST) {
             let newPost = {
             id: 5,
             message: this._state.profilePage.newPostText,
@@ -77,10 +80,24 @@ let store = {
             };
             this._state.dialogsPage.messageData.push(newMessage);
             this._rerenderEntireTree(this._state);
-        }else if (action.type === "UPDATE-NEW-POST-TEXT") {        
+        }else if (action.type === UPDATE_NEW_POST_TEXT) {        
             this._state.profilePage.newPostText = action.newText;
             this._rerenderEntireTree(this._state);
         }
+    }
+}
+
+
+export const addPostActionCreator = () => {
+  return {
+    type: ADD_POST
+  }
+}
+
+export const updateNewPostTextActionCreator = (text) => {
+  return {
+      type: UPDATE_NEW_POST_TEXT,
+      newText: text,
     }
 }
 
