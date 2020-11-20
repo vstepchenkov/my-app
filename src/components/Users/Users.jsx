5249ea34f47643cb35b22f3a49bfa685b06a89fe
vstepchenkov <mail2blast@gmail.com>
2020-11-20 15:52:04 +0200

    ok

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,7 +3,23 @@ import React from "react";
 let Users = (props) => {
     return (
         <div>
-            Users
+            <div>Users</div>
+            <div>
+                <span>
+                    <div><img src="" alt="" /></div>
+                    <button>Follow</button>
+                </span>
+                <span>
+                    <span>
+                        <div>FullName</div>
+                        <div>Status</div>
+                    </span>
+                    <span>
+                        <div>Country</div>
+                        <div>City</div>
+                    </span>
+                </span>
+            </div>
         </div>
     )
 }
